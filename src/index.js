0dc47812dea5f919aa188fea69adc36a24973bb1
Vitalii <vetingof@gmail.com>
2022-10-25 22:49:48 +0300

    HW#10

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
 console.log("Launching script");
 const anchors = document.querySelectorAll(".thumbnails-anchor");
 const detailsImage = document.querySelector(".details-image");
-const detailsAudio = document.querySelector(".details-audio");
+const audio = document.querySelector(".audio-sound");
 const detailsTitle = document.querySelector(".details-title");
 const mainClass = document.querySelector(".main-class");
 const detailsContainer = document.querySelector(".details-container");
 const HIDDEN = "hidden";
 const IS_POINT = "is-point";
+
 function setDetails(anchor) {
     detailsImage.setAttribute('src', anchor.getAttribute('data-details-image'));
-    detailsSound.setAttribute('src', anchor.getAttribute('data-details-sound'));
     detailsTitle.innerHTML = anchor.getAttribute('data-details-title');
+    pauseAudio(1);
+    audio.setAttribute('src', anchor.getAttribute('data-details-audio'));
 }
 for(let i = 0; i < anchors.length; i++) {
     anchors[i].addEventListener("click", function() {
         console.log("event - click on ", anchors[i]);
-        showDetails();
         setDetails(anchors[i]);
+        showDetails();
     })
 }
     function showDetails() {
@@ -24,8 +26,21 @@ for(let i = 0; i < anchors.length; i++) {
         detailsContainer.classList.add(IS_POINT);
         setTimeout(function() {
             detailsContainer.classList.remove(IS_POINT);
-        },1)
+        },1);
+        playAudio();
+        pauseAudio(5000);
     }
     function hideDetails() {
         mainClass.classList.add(HIDDEN);
+        pauseAudio(1);
+    }
+    function playAudio() {
+        setTimeout(function() {
+            audio.play();
+        }, 1);
+    }
+    function pauseAudio(msecs) {
+        setTimeout(function() {
+            audio.pause(msecs);
+        }, 1);
     }
