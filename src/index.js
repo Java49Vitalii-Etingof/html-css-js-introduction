56c14abebce4ea6b4a9a30b2a4eea3e44cb1ab3a
Vitalii <vetingof@gmail.com>
2022-10-26 12:44:53 +0300

    HW#10

--- src/index.js
+++ src/index.js
@@ -10,37 +10,43 @@ const IS_POINT = "is-point";
 
 function setDetails(anchor) {
     detailsImage.setAttribute('src', anchor.getAttribute('data-details-image'));
-    detailsTitle.innerHTML = anchor.getAttribute('data-details-title');
     pauseAudio(1);
-    audio.setAttribute('src', anchor.getAttribute('data-details-audio'));
+    audio.setAttribute("src", anchor.getAttribute('data-details-audio'));
+    detailsTitle.innerHTML = anchor.getAttribute('data-details-title');
 }
-for(let i = 0; i < anchors.length; i++) {
-    anchors[i].addEventListener("click", function() {
+
+for (let i = 0; i < anchors.length; i++) {
+    anchors[i].addEventListener("click", function () {
         console.log("event - click on ", anchors[i]);
         setDetails(anchors[i]);
         showDetails();
     })
 }
-    function showDetails() {
-        mainClass.classList.remove(HIDDEN);
-        detailsContainer.classList.add(IS_POINT);
-        setTimeout(function() {
-            detailsContainer.classList.remove(IS_POINT);
-        },1);
-        playAudio();
-        pauseAudio(5000);
-    }
-    function hideDetails() {
-        mainClass.classList.add(HIDDEN);
-        pauseAudio(1);
-    }
-    function playAudio() {
-        setTimeout(function() {
-            audio.play();
-        }, 1);
-    }
-    function pauseAudio(msecs) {
-        setTimeout(function() {
-            audio.pause(msecs);
-        }, 1);
-    }
+
+function showDetails() {
+    mainClass.classList.remove(HIDDEN);
+    detailsContainer.classList.add(IS_POINT);
+    setTimeout(function () {
+        detailsContainer.classList.remove(IS_POINT);
+    }, 1);
+
+    playAudio();
+    pauseAudio(5000);
+}
+function hideDetails() {
+    mainClass.classList.add(HIDDEN);
+    pauseAudio(1);
+}
+
+function playAudio() {
+    setTimeout(function () {
+        audio.play();
+    }, 1);
+}
+
+function pauseAudio(msec) {
+    setTimeout(function () {
+        audio.pause();
+    }, msec);
+}
+
