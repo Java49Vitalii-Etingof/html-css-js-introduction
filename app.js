acfac8032701031695da0fcb408db030b6e0d1c2
Vitalii <vetingof@gmail.com>
2022-10-28 11:48:33 +0300

    CW#11

---
+++ app.js
@@ -0,0 +1,14 @@
+
+// console.log("Hello world");
+// for (var i = 0; i < 3; i++) {
+//     setTimeout(function(){
+//         console.log(i)});
+// }
+// for (let i = 0; i < 3; i++) {
+//     setTimeout(function(){console.log(i)});
+// }
+function sum(op1, op2) {
+    let res = op1 + op2;
+    return res;
+}
+console.log(sum(10, 20))
