987907d24fcc334323cb3d7d7376dc65abc35558
Vitalii <vetingof@gmail.com>
2022-11-02 13:47:51 +0200

    HW#12

--- app.js
+++ app.js
@@ -156,4 +156,32 @@ console.log(fromNumberToString(-65656, 16));
 console.log(fromNumberToString(11483, 2));
 console.log(fromNumberToString(456888, 37));
 console.log(fromNumberToString('Word', 36));
+//******************************************************************
+console.log('function fromStringToNumber(string, base)');
+function fromStringToNumber(string, base) {
+    if(typeof base != 'number' || base<2 || base>36) {
+        return 'base is not valid';
+    }
+    if(typeof string != 'string') {
+        return 'string is not string';
+    }
+ let result = 0; 
+ for(let i=0; i<string.length; i++) {
+    let code = string.charCodeAt(i);
+    if(code >= 48 && code <= 57) {
+        code -= 48;
+    } else if(code >= 97 && code <= 122) {
+        code -= 87;
+    } else {
+        return 'error';
+    }//abc(x) = (a*(x**2) + b*(x**1) + c*(x**0))/10 Binary to decimal conversion example
+    let n = string.length -(i + 1);
+    result = result + code * (base**n);
+ } 
+ return result;
+}
+console.log(fromStringToNumber('java', 37));
+console.log(fromStringToNumber(32575, 36));
+console.log(fromStringToNumber('java', 36));
+console.log(fromStringToNumber('10110011011011', 2));
 
