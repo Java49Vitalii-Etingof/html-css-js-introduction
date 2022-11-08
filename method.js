d91c4af17ddfac1bae398d88b9b93601d8fec101
Vitalii <vetingof@gmail.com>
2022-11-09 01:47:04 +0200

    hw#14

--- method.js
+++ method.js
@@ -1,22 +1,22 @@
 let strings = []
 let ulSurrounding = []
 function ulSurround(strings) {
-    strings = strings.map(function(symbol, index) {
-        return '<li>' + symbol + '</li>';   
+    strings = strings.map(function (symbol, index) {
+        return '<li>' + symbol + '</li>';
     })
     strings.splice(0, 0, '<ul>');
     strings.push('</ul>');
     return strings
 }
-console.log('Converted array =>', ulSurround(['hlk','shu','frt']));
+console.log('Converted array =>', ulSurround(['hlk', 'shu', 'frt']));
 
 //************************************************* */
 
 
 function count(strings, symbol) {
-    return strings.reduce(function(count, value) {
-        return value === symbol ? count+1 : count;
-    }, 0);   
+    return strings.reduce(function (count, value) {
+        return value === symbol ? count + 1 : count;
+    }, 0);
 }
 console.log('element match ', count(['dz', 'qt', 'ke', 'dz', 'qt'], 'dz'));
 console.log('element match ', count(['dz', 'qt', 'ke', 'dz', 'qt'], 'qt'));
@@ -24,4 +24,16 @@ console.log('element match ', count(['dz', 'qt', 'ke', 'dz', 'qt'], 'qt'));
 //**************************************************/
 
 
+function arrayCopy(src, srcPos, dst, dstPos, length) {
 
+    src.slice(srcPos, srcPos + length)
+        .reverse().forEach(value => {
+            dst.splice(dstPos, 0, value);
+        });
+}
+
+let src = [38, 245, 1, 3, 45, 2, 7, 9, 0];
+let dst = [58, 14, 6, 8, 9, 0, 0, 2, 23, 5];
+
+arrayCopy(src, 2, dst, 4, 2);
+console.log('converted array', dst);
