21cc70ff9477bb033ae44a6df28ab770f0203940
Vitalii <vetingof@gmail.com>
2022-11-21 17:27:35 +0200

    HW#18

--- app.js
+++ app.js
@@ -1,20 +1,66 @@
+// function getAddressKey() {
+   
+//     return "address";
+// }
 // const person = {id: 123, first_name: 'Vasya',
-//  last_name: 'Ivanov', year: 2000, addres: {city: 'Lod',
-// street: 'Sokolov', app: 100}
+//  last_name: 'Ivanov', year: 2000, address: {city: 'Lod',
+//   street: 'Sokolov', app: 100}
 // };
-// const yearExp = "ye" +"ar";
-// console.log(person[yearExp]);
-function displayOccurrences(strings) {
-    const occurrences = {};
-    strings.array.forEach (element => {
-       if (occurrences[element]) {
-        occurrences[element] ++;
-       } else {
-        occurrences[element] = 1;
-       }
-    });
-        
-   console.log(occurrences); 
+// const name1 = getAddressKey();
+
+// console.log(person[name1])
+// const personArr = [123, 'Vasya', 'Ivanov', 2000,
+//  ['Lod', 'Sokolov', 100]]
+//  console.log(personArr[3])
+//  console.log(person.address);
+//******************************************* */
+// function displayOccurrences(strings) {
+//    const occurrences = {};
+//    strings.forEach(element => {
+//        if (occurrences[element]) {
+//            occurrences[element]++;
+//        } else {
+//            occurrences[element] = 1;
+//        }
+//    });
+//    //console.log(Object.entries(occurrences));
+//    const occurrencesAr = Object.entries(occurrences)
+//    occurrencesAr.sort((e1, e2) => e2[1] - e1[1])
+//    console.log(occurrencesAr);
+
+// }
+//*************************************************** */
+//  const x = {};
+//  const string = "abc";
+//  x[string] = 1;
+//  console.log(x);
+//  x[string]++;
+//  console.log(x);
+//const strings = ["a","opr","lmn", "abc", "lmn","abc", "lmn", "lmn", "abc", "a"];
+//displayOccurrences(strings);
+//********************************************************************************* */
+const string1 = 'yellow';
+const str1 = Array.from(string1);
+const string2 = 'yello';
+const str2 = Array.from(string2);
+console.log(str1);
+console.log(str2);
+function isAnagram()
+if(str1.length != str2.length){
+   res = false
+   console.log(res);
 }
-const strings = ['lmn', 'abc', 'lmn', 'lmn', 'abc', 'a']
-displayOccurrences(strings);
+
+function displayOccurrences(strings) {
+      const occurrences = {};
+    strings.forEach(element => {
+        if (occurrences[element]) {
+           occurrences[element]++;
+         } else {
+              occurrences[element] = 1;
+        }
+      });
+      console.log(Object.entries(occurrences));
+   }
+   
+displayOccurrences(str1);
