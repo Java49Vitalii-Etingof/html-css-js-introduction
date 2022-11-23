07fadd8c4e571894a51b9a3eafe9a7eb176075c0
Vitalii <vetingof@gmail.com>
2022-11-23 17:42:04 +0200

    HW#18

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // function getAddressKey() {
-   
+
 //     return "address";
 // }
 // const person = {id: 123, first_name: 'Vasya',
@@ -42,45 +42,45 @@
 // displayOccurrences(strings);
 //************************isAnagram******************************************** */
 const str1 = 'Yellow';
-const str2 = 'Yellow';
-//****************************************** 
+const str2 = 'Yelolw';
 
-function isAnagram(str1,str2) {
-    if(str1.length != str2.length){
-        //res = 'input parameters must be the same length';
-        return false;
-    }
-let ar1 = Array.from(str1.toLowerCase());
-let ar2 = Array.from(str2.toLowerCase());
+function isAnagram(str1, str2) {
+  console.log(str1);
+  console.log(str2);
 
-let occurrences  = getOccurrences(ar1);  
- console.log(occurrences);
- for(let i=0; i<ar2.length; i++) {
-    if(occurrences[ar2[i]] == undefined) {
-     
+  if (str1.length != str2.length) {
+    console.log('input parameters must be the same length');
+    return false;
+  }
+  let Ar1 = Array.from(str1.toLowerCase());
+  let Ar2 = Array.from(str2.toLowerCase());
+
+  let obj1 = getOccurrences(Ar1);
+  console.log(obj1);
+  for (let i = 0; i < Ar2.length; i++) {
+    if (obj1[Ar2[i]] == undefined) {
+      console.log('letter "', Ar2[i], '" doesn’t exist');
       return false;
     }
-    if(--occurrences[ar2[i]] < 0) {
-     
+    if (--obj1[Ar2[i]] < 0) {
+      console.log('number of letters "', Ar2[i], '"in the word"', str2, 'does not match the number in the word"', str1);
       return false;
     }
   }
-  
   return true;
 }
 
-
 function getOccurrences(strings) {
-   const occurrences = {};
-   strings.forEach(element => {
-       if (occurrences[element]) {
-           occurrences[element]++;
-       } else {
-           occurrences[element] = 1;
-       }
-   });
-   return occurrences;
+  const occurrences = {};
+  strings.forEach(element => {
+    if (occurrences[element]) {
+      occurrences[element]++;
+    } else {
+      occurrences[element] = 1;
+    }
+  });
+  return occurrences;
 }
-   
-res =  isAnagram(str1, str2) ;
- console.log(res);
+
+res = isAnagram(str1, str2);
+console.log('word "', str2, '"');
