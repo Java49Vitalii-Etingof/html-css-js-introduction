6f1d3be11d73280ad3860b08fb16554818cf95e7
Vitalii <vetingof@gmail.com>
2022-11-25 22:44:50 +0200

    HW#19

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
  function createRandomEmployees(nEmployees, idDigits, minSalary, maxSalary, minYearOfBirth, maxYearOfBirth ){
- let employees = [];
-let idDigits = 4;
+ 
+
   minId = Math.pow(10, idDigits-1);
   maxId = Math.pow(10, idDigits);
-//employees.forEach(function(element) {
-  let id = getRandomIntegerValue(minId, maxId);
-  console.log(id);
-//employees.push(id)
-//});
-function getRandomIntegerValue(min, max) {
+  for(let i = 0; i < nEmployees; i++){
+    let id = getRandomId(minId, maxId);
+
+  }
+
+ function getRandomIntegerValue(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random()*(max-min)+min);
+}
+
+function createEmployee(id, name, yearOfBirth, salary ){
+  return {id, name, yearOfBirth, salary};
 }
 
 
@@ -19,4 +23,5 @@ function getRandomIntegerValue(min, max) {
 //   id = getRandomIntegerValue(min, max)
 //   return id;
 // }
-// }
+ }
+
