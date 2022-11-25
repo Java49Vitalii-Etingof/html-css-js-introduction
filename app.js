c833b15bb3b0a9a5c9d57f279448092c0d45fd13
Vitalii <vetingof@gmail.com>
2022-11-25 19:22:49 +0200

    HW#19

--- app.js
+++ app.js
@@ -1,17 +1,6 @@
-function createEmployee(id, name, birthYear, salary) {
-  return {id, name, birthYear, salary};
+function createRandomEmployees(nEmployees, idDigits, minSalary, maxSalary, minYearOfBirth, maxYearOfBirth ){
+
+  minId = Math.pow(10, idDigits-1);
+  maxId = Math.pow(10, idDigits);
+  
 }
-const empl1 = createEmployee(123, "Vasya", 2000, 10000);
-const empl2 = empl1;
-console.log(empl1==empl2);
-function updateSalary(empl1, newSalary) {
-  empl1 = createEmployee(123, "Vasya", 2000, newSalary)
-}
-updateSalary(empl1, 15000);
-console.log(empl1);
-function updateSalaryPrimitive(salary, newSalary) {
-  salary = newSalary;
-}
-let salary = 10000;
-updateSalaryPrimitive(salary, 20000);
-console.log("salary", salary);
