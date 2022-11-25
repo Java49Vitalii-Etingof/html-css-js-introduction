ab720b8abd8165d5ec7fdac8f9e411abe674e8d9
Vitalii <vetingof@gmail.com>
2022-11-25 19:50:10 +0200

    HW#19

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 function createRandomEmployees(nEmployees, idDigits, minSalary, maxSalary, minYearOfBirth, maxYearOfBirth ){
-
+let employees = [];
   minId = Math.pow(10, idDigits-1);
   maxId = Math.pow(10, idDigits);
+array.forEach(nEmployees => {
   
+});
 }
