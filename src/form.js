829d594bff9dd15de82b5b555ba5b09e99043e3a
Vitalii <vetingof@gmail.com>
2022-12-01 21:08:35 +0200

    HW#21

--- src/form.js
+++ src/form.js
@@ -5,13 +5,15 @@ function onSubmit(event) {
     const employee = Array.from(inputElements).reduce(
         (res, cur) => {
             res[cur.name] = cur.value;
-        },{}
+            return res;
+        }, {}
     )
-    console.log(employee);
+    console.log(employee)
 }
 function onChange(event) {
     if (event.target.name == "salary") {
-        if(+event.target.value<1000 || +event.target.value<40000)
-        event.target.value
+        if(+event.target.value < 1000 || +event.target.value > 40000){
+            event.target.value=''
+        }
     }
 }
