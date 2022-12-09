b7cb15773ca57d2881423371fd1a93abc64b8f2a
Vitalii <vetingof@gmail.com>
2022-12-09 17:33:50 +0200

    HW#23

--- src/main.js
+++ src/main.js
@@ -6,4 +6,28 @@ const ACTIVE = "active";
 const sectionsElement = document.querySelectorAll("section");
 const buttonsMenuElement = document.querySelectorAll(".buttons-menu*");
 
+function showSection(index) {
+    buttonsMenuElement.forEach(e => e.classList.remove(ACTIVE));
+    sectionsElement.forEach(e => e.hidden = true)
+    buttonsMenuElement[index].classList.add(ACTIVE);
+    sectionsElement[index].hidden = false;
+    // if (index == 1) {
+    //     const employees = company.getAllEmployees();
+    //     employeesListElement.innerHTML = getEmployeeItems(employees);
+    // }
+}
+
+function onSubmit(event) {
+    event.preventDefault();
+    console.log("submitted");
+    const book = Array.from(inputElements).reduce(
+        (res, cur) => {
+            res[cur.name] = cur.value;
+            return res;
+        }, {}
+    )
+    console.log(book)
+    // company.hireEmployee(employee);
+    
+}
 
