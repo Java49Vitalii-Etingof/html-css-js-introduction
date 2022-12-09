883eb016b9adf15576c01427522b996bf9d7b1ab
Vitalii <vetingof@gmail.com>
2022-12-09 17:07:13 +0200

    HW#23

---
+++ src/main.js
@@ -0,0 +1,9 @@
+
+
+const inputElements = document.querySelectorAll(".form-class [name]");
+const ACTIVE = "active";
+
+const sectionsElement = document.querySelectorAll("section");
+const buttonsMenuElement = document.querySelectorAll(".buttons-menu*");
+
+
