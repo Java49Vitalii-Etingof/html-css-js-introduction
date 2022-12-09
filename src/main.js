ae28810ef56ce5e62e43bf1edf1fe45c17a360a2
Vitalii <vetingof@gmail.com>
2022-12-09 19:39:14 +0200

    HW#23

--- src/main.js
+++ src/main.js
@@ -2,12 +2,17 @@
 
 const inputElements = document.querySelectorAll(".form-class [name]");
 const ACTIVE = "active";
+const MIN_PAGES = 50;
+const MAX_PAGES = 2000;
+const TIME_OUT_ERROR_MESSAGE = 5000;
+const ERROR_CLASS = "active"
 
 const sectionsElement = document.querySelectorAll("section");
-const buttonsMenuElement = document.querySelectorAll(".buttons-menu*");
+const buttonsMenuElement = document.querySelectorAll(".buttons-menu");
+const numberOfPagesErrorElement = document.getElementById("numberOfPages_error");
 
 function showSection(index) {
-    buttonsMenuElement.forEach(e => e.classList.remove(ACTIVE));
+     buttonsMenuElement.forEach(e => e.classList.remove(ACTIVE));
     sectionsElement.forEach(e => e.hidden = true)
     buttonsMenuElement[index].classList.add(ACTIVE);
     sectionsElement[index].hidden = false;
@@ -28,6 +33,30 @@ function onSubmit(event) {
     )
     console.log(book)
     // company.hireEmployee(employee);
+    
+}
+function onChange(event) {
+    if (event.target.name == "numberOfPages") {
+        validateNumberOfPages(event.target)
+    }
+}
+function validateNumberOfPages(element) {
+    const value = +element.value;
+    if (value < MIN_PAGES || value > MAX_PAGES) {
+        const message = value < MIN_PAGES ? `Number of the pages must be ${MIN_PAGES} or greater`: 
+        `Number of the pages must be ${MAX_PAGES} or less`; 
+        showErrorMessage(element, message, numberOfPagesErrorElement );
+    }
+}
+function showErrorMessage(element, message, errorElement) {
+    element.classList.add(ERROR_CLASS);
+    errorElement.innerHTML = message;
+    setTimeout(() =>{
+        element.classList.remove(ERROR_CLASS);
+        element.value = '';
+        errorElement.innerHTML = '';
+    }, TIME_OUT_ERROR_MESSAGE);
+
     
 }
 
