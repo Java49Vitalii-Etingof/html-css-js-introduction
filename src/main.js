bf22278e5a78ad82b4aef189d7c5b777b09dbe8e
Vitalii <vetingof@gmail.com>
2022-12-10 20:25:47 +0200

    HW#23

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ const inputElements = document.querySelectorAll(".form-class [name]");
 const ACTIVE = "active";
 const MIN_PAGES = 50;
 const MAX_PAGES = 2000;
-const MIN_DATE = '1980-01-01';
-const MAX_DATE = 
+const MIN_DATE = new Date('1980-01-01');
+const MAX_DATE = new Date();
 const TIME_OUT_ERROR_MESSAGE = 5000;
 const ERROR_CLASS = "error";
 
@@ -13,16 +13,42 @@ const sectionsElement = document.querySelectorAll("section");
 const buttonsMenuElement = document.querySelectorAll(".buttons-menu *");
 const numberOfPagesErrorElement = document.getElementById("numberOfPages_error");
 const publishingDateErrorElement = document.getElementById("publishingDate_error");
+const booksListElement = document.getElementById("books-all");
+
+const libraryb = new Libraryb();
+class Libraryb {
+    constructor() {
+        this.books = []
+    }
+    getAllBooks() {
+        return this.books;
+    }
+}
 function showSection(index) {
      buttonsMenuElement.forEach(e => e.classList.remove(ACTIVE));
     sectionsElement.forEach(e => e.hidden = true)
     buttonsMenuElement[index].classList.add(ACTIVE);
     sectionsElement[index].hidden = false;
-    // if (index == 1) {
-    //     const employees = company.getAllEmployees();
-    //     employeesListElement.innerHTML = getEmployeeItems(employees);
-    // }
+     if (index == 1) {
+     const books = libraryb.getAllBooks();
+     booksListElement.innerHTML = getbookItems(books);
+     }
 }
+function getbookItems(books) {
+    return books.map (e =>
+        `<li class="books-item">
+            <div class = "books-item-conteiner">
+                <p class ="books-item-paragraph">Book title: ${e.book_title} </p>
+                <p class ="books-item-paragraph">Author: ${e.author} </p>
+                <p class ="books-item-paragraph">Genre: ${e.genre} </p>
+                <p class ="books-item-paragraph">Number of pages: ${e.numberOfPages} </p>
+                <p class ="books-item-paragraph">Publishing date: ${e.publishingDate} </p>
+            </div>
+
+        </li>`).join('');
+        
+    }
+
 
 function onSubmit(event) {
     event.preventDefault();
@@ -45,6 +71,15 @@ function onChange(event) {
         validatePublishingDate(event.target)
     }
 }
+    function validatePublishingDate(element) {
+        const value = new Date(element.value);
+        if (value < MAX_DATE || value > MAX_DATE) {
+        const message = value < MIN_DATE ? `Publishing date must be ${MIN_DATE} or greater`:
+        `Publishing date must be ${MAX_DATE} or less`;
+        showErrorMessage(element, message, publishingDateErrorElement);    
+        }
+    }
+
 function validateNumberOfPages(element) {
     const value = +element.value;
     if (value < MIN_PAGES || value > MAX_PAGES) {
